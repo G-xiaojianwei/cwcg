ea24f8aac35cb3c9792a851a78f112fbea0de7d1
G-xiaojianwei <40557962+G-xiaojianwei@users.noreply.github.com>
2021-05-19 10:31:17 +0800

    Add files via upload

---
+++ filter/filterAll.js
@@ -0,0 +1,11 @@
+export const genderMenu = {
+    strJq:function(v){
+    	return 1;
+    	console.log(v.replace("诚外诚购-",""))
+    	return v.replace("诚外诚购-","");
+    }
+}
+
+export default {
+    GENDER_MENU, COMMON_MENU
+}
